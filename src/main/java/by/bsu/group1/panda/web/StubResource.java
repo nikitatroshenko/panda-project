699a7bce628eb76400562529dec27e7c1b60adec
nikitatroshenko <nikitatroshenko@gmail.com>
2017-12-16 02:34:04 +0300

    Added stub resource
    Added a stub resource that does nothing but showing user a predefined message

---
+++ src/main/java/by/bsu/group1/panda/web/StubResource.java
@@ -0,0 +1,17 @@
+package by.bsu.group1.panda.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StubResource {
+
+	@Value("panda.message.stub")
+	private String stubMessage;
+
+	@RequestMapping("/")
+	String index() {
+		return stubMessage;
+	}
+}
