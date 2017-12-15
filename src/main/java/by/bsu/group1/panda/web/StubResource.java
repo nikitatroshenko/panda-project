f2794b2efbd4e5958f2c19bd693a379cddd05a0e
nikitatroshenko <nikitatroshenko@gmail.com>
2017-12-16 02:49:17 +0300

    Fixed stub resource message displaying

--- src/main/java/by/bsu/group1/panda/web/StubResource.java
+++ src/main/java/by/bsu/group1/panda/web/StubResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StubResource {
 
-	@Value("panda.message.stub")
+	@Value("${panda.message.stub}")
 	private String stubMessage;
 
 	@RequestMapping("/")
